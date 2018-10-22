3f5db60d1c3a762f53fd2ff1dfa284aa46dbc1c3
苏蕊 <surui@qijiayoudao.com>
2018-10-22 19:22:22 +0800

    sass

--- config-overrides.js
+++ config-overrides.js
@@ -12,6 +12,39 @@ module.exports = function override(config, env) {
         style: true, // use less for customized theme
     }], config);
     console.log(config.module.rules[2].oneOf);
+
+    // sass
+    config.module.rules[2].oneOf.unshift(
+        {
+            test: /\.scss$/,
+            use: [
+                require.resolve('style-loader'),
+                require.resolve('css-loader'),
+                require.resolve('sass-loader'),
+                {
+                    loader: require.resolve('postcss-loader'),
+                    options: {
+                        // Necessary for external CSS imports to work
+                        // https://github.com/facebookincubator/create-react-app/issues/2677
+                        ident: 'postcss',
+                        plugins: () => [
+                            require('postcss-flexbugs-fixes'),
+                            autoprefixer({
+                                browsers: [
+                                    '>1%',
+                                    'last 4 versions',
+                                    'Firefox ESR',
+                                    'not ie < 9', // React doesn't support IE8 anyway
+                                ],
+                                flexbox: 'no-2009',
+                            })
+                        ],
+                    },
+                }
+            ]
+        }
+    );
+    //less
     config.module.rules[2].oneOf.unshift(
         {
             test: /\.less$/,
@@ -51,7 +84,7 @@ module.exports = function override(config, env) {
 
     // file-loader exclude
     let l = getLoader(config.module.rules, fileLoaderMatcher);
+    l.exclude.push(/\.scss$/);
     l.exclude.push(/\.less$/);
-
     return config;
 };
